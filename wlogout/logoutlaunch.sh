4de1c5b147d6935b8b7ea172a7953046c59ce4af
Archit <architdakhore@acpce.ac.in>
2026-03-01 00:06:51 +0530

    Add files via upload

---
+++ wlogout/logoutlaunch.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#// Check if wlogout is already running
+
+if pgrep -x "wlogout" >/dev/null; then
+    pkill -x "wlogout"
+    exit 0
+fi
+
+#// set file variables
+
+scrDir="$/etc/wlogout"
+wlogoutStyle=1
+wlogoutStyle=${wlogoutStyle:-$WLOGOUT_STYLE}
+confDir="${confDir:-$HOME/.config}"
+wLayout="${confDir}/wlogout/layout_${wlogoutStyle}"
+wlTmplt="${confDir}/wlogout/style_${wlogoutStyle}.css"
+
+if [ ! -f "${wLayout}" ] || [ ! -f "${wlTmplt}" ]; then
+    echo "ERROR: Config ${wlogoutStyle} not found..."
+    wlogoutStyle=1
+    wLayout="${confDir}/wlogout/layout_${wlogoutStyle}"
+    wlTmplt="${confDir}/wlogout/style_${wlogoutStyle}.css"
+fi
+#// scale config layout and style
+
+wlColms=4
+
+#// eval config files
+
+wlStyle="$(envsubst <"${wlTmplt}")"
+
+#// launch wlogout
+
+wlogout -b "${wlColms}" -c 0 -r 0 -m 0 --layout "${wLayout}" --css <(echo "${wlStyle}") --protocol layer-shell
